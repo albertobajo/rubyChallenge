3fb50854e0bda7a26d471dbcd46d6b5cb8a927a5
Alberto Bajo <albertobajo@gmail.com>
2019-01-06 23:36:42 +0100

    [Refactor] Checkout & LineItem

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,37 +1,28 @@
 require_relative 'line_item'
 
 class Checkout
-  attr_accessor :pricing_rules
-  attr_reader :items
-
   def initialize(pricing_rules)
-    @pricing_rules = pricing_rules
-    @items = []
+    @line_items = Hash.new
+
+    pricing_rules.each do |rule|
+      li = LineItem.new
+      li.code = rule["code"]
+      li.quantity = 0
+      li.price = rule["price"].gsub(/[^\d\.]/, '').to_f
+      li.discount_bulk = rule["discount_bulk"] === "true"
+      li.discount_bulk_price = rule["discount_bulk_price"].gsub(/[^\d\.]/, '').to_f
+      li.discount_bulk_minimum_quantity = rule["discount_bulk_minimum_quantity"].to_i
+      li.discount_two_for_one = rule["discount_2for1"] === "true"
+
+      @line_items[li.code] = li
+    end
   end
 
-  def scan(item_code)
-    @items << item_code
+  def scan(code)
+    @line_items[code].quantity += 1
   end
 
   def total
-    items = Hash[@items.group_by(&:itself).map {|k,v| [k, v.size] }]
-    total_price = 0.0
-
-    items.each do |item_code, quantity|
-      li = LineItem.new
-      pricing_rule = @pricing_rules.detect { |o| o["code"] === item_code }
-
-      li.code = item_code
-      li.quantity = quantity
-      li.price = pricing_rule["price"].gsub(/[^\d\.]/, '').to_f
-      li.discount_bulk = pricing_rule["discount_bulk"] === "true"
-      li.discount_bulk_price = pricing_rule["discount_bulk_price"].gsub(/[^\d\.]/, '').to_f
-      li.discount_bulk_minimum_quantity = pricing_rule["discount_bulk_minimum_quantity"].to_i
-      li.discount_two_for_one = pricing_rule["discount_2for1"] === "true"
-
-      total_price += li.total_price
-    end
-
-    return "#{sprintf('%.2f', total_price)}€"
+    "%.2f€" % @line_items.values.map(&:total_price).reduce(:+)
   end
 end
--- lib/line_item.rb
+++ lib/line_item.rb
@@ -8,23 +8,17 @@ class LineItem
   attr_accessor :quantity
 
   def total_price
-    final_price = nil
-
     raise TypeError unless price.is_a? Float
-    raise TypeError unless discount_bulk_price.nil? or discount_bulk_price.is_a? Float
+    raise TypeError unless
+      discount_bulk_price.nil? or discount_bulk_price.is_a? Float
 
     if discount_two_for_one
-      final_quantity = (quantity.to_f / 2).ceil
-      final_price = final_quantity * price
+      chargeable_items = (quantity.to_f / 2).ceil
+      chargeable_items * price
     elsif discount_bulk and quantity >= discount_bulk_minimum_quantity
-      final_price = quantity * discount_bulk_price
+      quantity * discount_bulk_price
     else
-      final_price = quantity * price
+      quantity * price
     end
-
-    return final_price
   end
-
-  # def initialize()
-  # end
 end
